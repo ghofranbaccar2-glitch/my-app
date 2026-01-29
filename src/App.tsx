602317404254dd8c2edecae828349c2895fc1cb9
ghofran <ghofranbaccar2@gmail.com>
2026-01-29 10:07:46 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Greeting from './Greeting';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Greeting name="World" />
+    </div>
+  );
+}
+
+export default App;
